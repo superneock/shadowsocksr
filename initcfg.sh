dc3421693152ed860c481c811ec35eb88cffbf6f
Neock <neocksu@gmail.com>
2018-03-07 10:51:20 +0800

    startup scripts

--- initcfg.sh
+++ initcfg.sh
@@ -5,4 +5,5 @@ chmod +x shadowsocks/*.sh
 cp -n apiconfig.py userapiconfig.py
 cp -n config.json user-config.json
 cp -n mysql.json usermysql.json
-
+cp -n shadowsocks_start_up /etc/init.d/shadowsocks
+chmod 755 /etc/init.d/shadowsocks ; update-rc.d shadowsocks defaults
